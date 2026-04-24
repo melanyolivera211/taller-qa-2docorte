8dcc4d84c70ffdbe08fbb1b6c62119d9fd87dd00
melanyolivera211 <melany.oliveraatencio@unicolombo.edu.co>
2026-04-24 16:47:31 -0500

    refactor: aplica Regla 4 y 10 de Guia1 (Naming y textos hardcoded) en ConsoleIO

--- src/main/java/com/jcaa/usersmanagement/application/port/in/UpdateUserUseCase.java
+++ src/main/java/com/jcaa/usersmanagement/application/port/in/UpdateUserUseCase.java
@@ -1,6 +1,9 @@
 package com.jcaa.usersmanagement.application.port.in;
 
 import com.jcaa.usersmanagement.application.service.dto.command.UpdateUserCommand;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
 public interface UpdateUserUseCase {
   void execute(@NotNull @Valid UpdateUserCommand command);
 }
